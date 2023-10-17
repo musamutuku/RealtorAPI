b4f9e5672aa57ac37f359dd4e2d637c8c162232d
josemusa <musajoseph2017@gmail.com>
2023-10-17 15:49:17 +0300

    get gata updates

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ app.post('/api/submit', async (req, res) => {
   const newEmail = req.body.userEmail;
   const newPhone = req.body.userPhone;
   const newMessage = req.body.userMessage;
+  const houseId = req.body.id;
 
   try {
-    const result = await pool.query('INSERT INTO public.users(username,email,phone,message) VALUES ($1,$2,$3,$4) RETURNING *', [newName, newEmail, newPhone, newMessage])
+    const result = await pool.query('INSERT INTO public.users(username,email,phone,message,houseID) VALUES ($1,$2,$3,$4,$5) RETURNING *', [newName, newEmail, newPhone, newMessage,houseId])
     // const insertedRow = result.rows[0].username;
     res.status(201).json({ loadingMsg: false,returnMsg1: 'Send successfully', returnImg: 'src/assets/images/success_img.png', returnMsg2: 'You will get a feedback after 24 hours', returnColor: 'black' });
   } catch (error) {
@@ -32,18 +33,44 @@ app.post('/api/submit', async (req, res) => {
     res.status(500).json({ loadingMsg: false, returnMsg1: 'An error occcured!', returnImg: 'src/assets/images/error_img.png', returnMsg2: 'Try again later', returnColor: 'brown' });
   }
 })
-// app.get('/api/data', (req, res) => {
-//   // Query the database using the connection pool
-//   pool.query('SELECT * FROM users', (error, result) => {
-//     if (error) {
-//       console.error('Error executing query', error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     } else {
-//       res.json(result.rows);
-//       console.log(result.rows)
-//     }
-//   });
+
+app.get('/api/data', (req, res) => {
+  // Query the database using the connection pool
+  pool.query('SELECT * FROM apartments', (error, result) => {
+    if (error) {
+      console.error('Error executing query', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+app.get('/api/data1', (req, res) => {
+  // Query the database using the connection pool
+  pool.query('SELECT * FROM townhouses', (error, result) => {
+    if (error) {
+      console.error('Error executing query', error);
+      res.status(500).json({ error: 'Internal Server Error'});
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+// get list data from table and return array list(testing)
+// app.get('/api/data2', async (req, res) => {
+//   try {
+//     const { rows } = await pool.query('SELECT price FROM apartments');
+//     const data = rows.map(row => row.price);
+//     res.json(data);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
 // });
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
